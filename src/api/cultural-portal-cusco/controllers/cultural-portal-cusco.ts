6e2010cf86defa1c625d3d041da42a8eb718ee45
Deployment <root@nick.panormov.fvds.ru>
2024-11-17 03:44:30 +0300

    Added content types

---
+++ src/api/cultural-portal-cusco/controllers/cultural-portal-cusco.ts
@@ -0,0 +1,7 @@
+/**
+ * cultural-portal-cusco controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cultural-portal-cusco.cultural-portal-cusco');
---
+++ src/api/cultural-portal-cusco/routes/cultural-portal-cusco.ts
@@ -0,0 +1,7 @@
+/**
+ * cultural-portal-cusco router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cultural-portal-cusco.cultural-portal-cusco');
