86015e0fceb4fc3174f04f95bf96cac5079413fe
Никита Павленко <nick.pavlenko1998@gmail.com>
2024-12-13 15:57:49 -0800

    feat(CPC-XX): Added filtration for events in collection

--- src/api/events-collection/controllers/events-collection.ts
+++ src/api/events-collection/controllers/events-collection.ts
@@ -4,4 +4,22 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::events-collection.events-collection');
+export default factories.createCoreController('api::events-collection.events-collection', (strapi) => ({
+  // Переопределяем стандартный метод find
+  async find(ctx) {
+    // Вызываем стандартное поведение find
+    const { data, meta } = await super.find(ctx);
+
+    // Добавляем свою логику
+    const filteredData = data.map(collection => ({
+      ...collection,
+      events: collection.events.filter(event =>
+        new Date(event.eventDate) >= new Date(ctx.query.filters.eventDate.$gte)
+      ),
+    }));
+
+    // Возвращаем изменённые данные
+    return { data: filteredData, meta };
+  },
+}));
+
