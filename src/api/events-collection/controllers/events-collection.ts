53910351b9515d35e260bcffce84599a63453dfe
Никита Павленко <nick.pavlenko1998@gmail.com>
2024-12-19 08:10:19 -0800

    chore(CPC-XX): Fixed Gallery issues

--- src/api/events-collection/controllers/events-collection.ts
+++ src/api/events-collection/controllers/events-collection.ts
@@ -4,24 +4,5 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::events-collection.events-collection', (strapi) => ({
-  // Переопределяем стандартный метод find
-  async find(ctx) {
-    // Вызываем стандартное поведение find
-    const { data, meta } = await super.find(ctx);
-
-    // Добавляем свою логику
-    const filteredData = data.map(collection => ({
-      ...collection.attributes,
-      attributes: {
-        events: collection.attributes.events.filter(event =>
-          new Date(event.data.attributes.eventDate) >= new Date(ctx.query.filters.eventDate.$gte)
-        ),
-      }
-    }));
-
-    // Возвращаем изменённые данные
-    return { data: filteredData, meta };
-  },
-}));
+export default factories.createCoreController('api::events-collection.events-collection');
 
