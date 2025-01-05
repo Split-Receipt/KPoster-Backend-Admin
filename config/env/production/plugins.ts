c3c91639358943633c6d7a02769ca5226c1a9eee
Никита Павленко <nick.pavlenko1998@gmail.com>
2025-01-05 09:45:44 -0800

    chore(CPC-XX): Added sendgrid as email provider

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -1,20 +1,37 @@
+// export default ({ env }) => ({
+// 	email: {
+// 		config: {
+// 		  provider: 'nodemailer',
+// 		  providerOptions: {
+// 			host: env('SMT_SERVER'),
+//             port: env('SMT_PORT'),
+//             secure: env.bool('SMT_SECURE'),
+// 			auth: {
+// 			  user: env('SMT_USER'),
+// 			  pass: env('SMT_PASS'),
+// 			},
+// 		  },
+// 		  settings: {
+// 			defaultFrom: env('SMT_USER'),
+//             defaultReplyTo: env('SMT_USER'),
+// 		  },
+// 		},
+// 	  },
+// });
+
+
 export default ({ env }) => ({
-	email: {
-		config: {
-		  provider: 'nodemailer',
-		  providerOptions: {
-			host: env('SMT_SERVER'),
-            port: env('SMT_PORT'),
-            secure: env.bool('SMT_SECURE'),
-			auth: {
-			  user: env('SMT_USER'),
-			  pass: env('SMT_PASS'),
-			},
-		  },
-		  settings: {
-			defaultFrom: env('SMT_USER'),
-            defaultReplyTo: env('SMT_USER'),
-		  },
-		},
-	  },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: env('SMT_USER'),
+        defaultReplyTo: env('SMT_USER'),
+        testAddress: env('SMT_USER'),
+      },
+    },
+  },
 });
