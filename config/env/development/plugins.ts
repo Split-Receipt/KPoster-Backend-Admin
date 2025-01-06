81d30de2e7e6a475c53022d508bf01f3c432554d
Никита Павленко <nick.pavlenko1998@gmail.com>
2025-01-06 12:54:21 -0800

    chore(CPC-XX): Added resend provider

--- config/env/development/plugins.ts
+++ config/env/development/plugins.ts
@@ -1,36 +1,14 @@
-// export default ({ env }) => ({
-// 	email: {
-// 		config: {
-// 		  provider: 'nodemailer',
-// 		  providerOptions: {
-// 			host: env('SMT_SERVER'),
-//             port: env('SMT_PORT'),
-//             secure: env.bool('SMT_SECURE'),
-// 			auth: {
-// 			  user: env('SMT_USER'),
-// 			  pass: env('SMT_PASS'),
-// 			},
-// 		  },
-// 		  settings: {
-// 			defaultFrom: env('SMT_USER'),
-//             defaultReplyTo: env('SMT_USER'),
-// 		  },
-// 		},
-// 	  },
-// });
-
 export default ({ env }) => ({
   email: {
     config: {
-      provider: 'sendgrid',
+      provider: 'strapi-provider-email-resend',
       providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
+        apiKey: env('RESEND_API_KEY'),
       },
       settings: {
         defaultFrom: env('SMT_USER'),
         defaultReplyTo: env('SMT_USER'),
-        testAddress: env('SMT_USER'),
       },
-    },
+    }
   },
 });
